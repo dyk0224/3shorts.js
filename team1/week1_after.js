66433dc3e58daa517e6825a6caf25bc1e10928e8
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-06-01 16:36:00 +0900

    Update week1_after.js

--- team1/week1_after.js
+++ team1/week1_after.js
@@ -1,1 +1,15 @@
-
+function solution(ingredient) {
+    let answer = 0;
+    let stack = [];
+    
+    ingredient.map((i) => {
+        stack.push(i);
+        
+        const isHamburger = stack.slice(-4).join('') === "1231";
+        if (!isHamburger) return;
+        
+        answer++;
+        stack.splice(-4);
+    })
+	return answer;
+}
