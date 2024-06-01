87b3d941beddb142f1b40d83aeb41caf605e346b
dyk0224 <88474141+dyk0224@users.noreply.github.com>
2024-06-01 16:19:45 +0900

    Update week1_before.js

--- team1/week1_before.js
+++ team1/week1_before.js
@@ -1,1 +1,21 @@
-
+function solution(ingredient) {
+    let answer = 0;
+    let stack = [];
+    
+    for (let i=0; i<ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        
+        if (stack.length >= 4) {
+            const leng = stack.length-1;
+            let str = "" + stack[leng-3] + stack[leng-2] + stack[leng-1] + stack[leng];
+            if (str == "1231") {
+                answer++;
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+            }
+        }
+    }
+	return answer;
+}
