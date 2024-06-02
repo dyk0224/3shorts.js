f9bb5171ab5305058846cfa8ad208b39678a73bc
ylem76 <ylem76@gmail.com>
2024-06-03 03:57:56 +0900

    update team2 - week1

--- team2/week1_after.js
+++ team2/week1_after.js
@@ -1,1 +1,13 @@
+// 고민되는 지점 : 함수를 쪼개는 걸 좋아하는데
+// 나중에 너무 쪼개서 전체 코드가 더 가독성이 낮아질 수 있지 않은지 고민이 된다
 
+const getKNumber = (arr, command) => {
+  const [start, end, k] = [command[0] - 1, command[1], command[2] - 1];
+  const sorted = arr.slice(start, end).sort((a, b) => a - b);
+  return sorted[k];
+};
+
+function solution(array, commands) {
+  const answers = commands.map((command) => getKNumber(array, command));
+  return answers;
+}
--- team2/week1_before.js
+++ team2/week1_before.js
@@ -1,1 +1,15 @@
+// - 불필요한 변수가 많은 것 같다. 바로 리턴을 해도 되는데, 중괄호도 없어도 될 거 같다.
+// - command 관련 변수 이름이 없어서 뭐가뭔지 잘 모르겠다.
+// - 디스트럭쳐링으로 이름을 지어주는 게 좋을 것 같음.
 
+function solution(array, commands) {
+  const getKNumber = (arr, command) => {
+    const sorted = arr.slice(command[0] - 1, command[1]).sort((a, b) => a - b);
+    return sorted[command[2] - 1];
+  };
+
+  const answer = commands.map((command) => {
+    return getKNumber(array, command);
+  });
+  return answer;
+}
