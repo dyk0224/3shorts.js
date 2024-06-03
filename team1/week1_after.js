67f53924c49cd9f0c6fae85446c76c6f7aa7181b
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-06-03 15:07:37 +0900

    Update week1_after.js hanju

--- team1/week1_after.js
+++ team1/week1_after.js
@@ -14,3 +14,23 @@ function solution(ingredient) {
 	
     return answer;
 }
+
+-------------------------------------------------------------
+	//hanju
+function solution(ingredient) {
+    var answer = 0;
+    let stack = [];
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        if (stack.length >= 4) {
+            if (stack[stack.length - 4] === 1 &&
+                stack[stack.length - 3] === 2 &&
+                stack[stack.length - 2] === 3 &&
+                stack[stack.length - 1] === 1) {
+                stack.splice(stack.length - 4, 4);
+                answer++;
+            }
+        }
+    }   
+		return answer;
+}//배열은 가변적이기 때문에 요소를 추가하거나 제거할 때, 다시 선언하지 않고 기존배열을 변경하기 때문에 성능개선.
