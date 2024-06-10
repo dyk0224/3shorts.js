c1ca4c0c84275e88839401877714f74ed0521d25
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-06-10 22:25:36 +0900

    Update week2_after.js
    Add hanju

--- team1/week2_after.js
+++ team1/week2_after.js
@@ -24,3 +24,26 @@ function solution(skill, skill_trees) {
     
     return answer;
 }
+
+-------------------------
+
+//hanju
+function solution(skill, skill_trees) {
+    let answer = 0;
+    let skillArray = [...skill];
+    for(skill_tree of skill_trees){
+        let possible = 1;
+        let skillTree = [...skill_tree];
+        let steps = skillTree.filter(step => skillArray.includes(step));
+        console.log(steps);
+        for(let i=0; i<steps.length; i++){
+            if(steps[i] !== skillArray[i]) {
+                possible = 0;
+                break;
+            }
+        }
+        if(possible) answer++;
+    }
+    return answer;
+}
+    
