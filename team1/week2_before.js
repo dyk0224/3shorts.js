9d759c39635d2d8b1954c0b5fd45dfec63a095b4
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-06-10 22:24:22 +0900

    Update week2_before.js
    Add hanju's solution

--- team1/week2_before.js
+++ team1/week2_before.js
@@ -24,3 +24,62 @@ function solution(skill, skill_trees) {
     
     return answer;
 }
+
+--------------------------------------------
+
+    //한주
+    
+    for(skillTree of skill_trees){
+        let step = 0;
+        let impossible = 0;
+        for(let i=0; i<skillTree.length; i++){
+            impossible = 0;
+            if(skillTree[i] === skill[step]) step++;
+            else{
+                for(let j=0; j<skill.length; j++){
+                    if(skillTree[i] === skill[j] && step !== j) {
+                        impossible = 1; 
+                        break;
+                    }
+                }
+                if(i===skillTree.length-1) answer++;
+            }
+        }
+    }
+//미완 : 첫 접근. 계속 for문을 중첩해서 쓰게 되면서 뭔가 이상함을 직감.
+
+function solution(skill, skill_trees) {
+    let answer = 0;
+    for(skill_tree of skill_trees){
+        let skillTree = [...skill_tree];
+        let skillArray = [...skill];
+        let steps = skillTree.filter(step => skillArray.includes(step));
+        for(let i=0; i<steps.length; i++){
+            if(steps[i] !== skillArray[i]) break;
+            if(i === steps.length -1) answer++;
+        }
+    }
+    return answer;
+}
+//실패이유: skill에 없는 문자들로만 이루어진 skill_tree를 받을 경우 answer를 다루는 if문에 접근이 불가능.
+
+//최종
+function solution(skill, skill_trees) {
+    let answer = 0;
+    let skillArray = [...skill];
+    for(skill_tree of skill_trees){
+        let possible = 1;
+        let skillTree = [...skill_tree];
+        let steps = skillTree.filter(step => skillArray.includes(step));
+        console.log(steps);
+        for(let i=0; i<steps.length; i++){
+            if(steps[i] !== skillArray[i]) {
+                possible = 0;
+                break;
+            }
+        }
+        if(possible) answer++;
+    }
+    return answer;
+}
+    
