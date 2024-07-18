516a28dd09b99728f6630f64b0b5778773956a17
HanjuJung <166710330+HanjuJung@users.noreply.github.com>
2024-07-18 21:06:27 +0900

    Update week8_before.js

--- team1/week8_before.js
+++ team1/week8_before.js
@@ -17,23 +17,3 @@ function solution(progresses, speeds) {
     }
     return answer;
 }
----------------------------------------
-
-
-function solution(progresses, speeds) {
-    const goal = 100;
-    let answer = [];
-    let leftDays = progresses.map((progress, index) => Math.ceil((goal - progress)/speeds[index]));
-    let complete = 0;
-    let releaseDay = leftDays[0];
-    for(const day of leftDays){
-        if(day > releaseDay){
-            releaseDay = day;
-            answer.push(complete);
-            complete = 0;
-        }
-        complete++;
-    }
-    answer.push(complete);
-    return answer;
-}
